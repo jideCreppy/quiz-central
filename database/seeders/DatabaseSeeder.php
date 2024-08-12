83a0738a83598a877f4ddd68159e1043d09a996b
jide creppy <jide.creppy@gmail.com>
2024-08-12 17:01:18 +0100

    break seeders into individual files

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Category;
-use App\Models\Difficulty;
-use App\Models\QuizType;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,44 +12,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Category::factory()->create([ // Add more categories as needed. see https://opentdb.com/api_config.php for more values that can be added to the database.
-            'label' => 'Entertainment (Film)',
-            'value' => 11,
-        ]);
+        // Add more categories as needed. See https://opentdb.com/api_config.php for more values that can be added to the database.
 
-        Category::factory()->create([
-            'label' => 'Science & Nature',
-            'value' => 17,
-        ]);
-
-        Category::factory()->create([
-            'label' => 'History',
-            'value' => 23,
-        ]);
-
-        Difficulty::factory()->create([
-            'label' => 'Easy 😌',
-            'value' => 'easy',
-        ]);
-
-        Difficulty::factory()->create([
-            'label' => 'Medium 😥',
-            'value' => 'medium',
-        ]);
-
-        Difficulty::factory()->create([
-            'label' => 'Hard 😱',
-            'value' => 'hard',
-        ]);
-
-        QuizType::factory()->create([
-            'label' => 'True / False',
-            'value' => 'boolean',
-        ]);
-
-        QuizType::factory()->create([
-            'label' => 'Multiple Choice',
-            'value' => 'multiple',
+        $this->call([
+            CategorySeeder::class,
+            DifficultySeeder::class,
+            QuizTypeSeeder::class,
         ]);
     }
 }
